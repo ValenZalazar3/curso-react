2ce421851f3f84d8ec2fbce9fc0c631725d157a1
Valentin zalazar <valenzalazar3@gmail.com>
2023-08-28 22:52:56 -0300

    seccion3clase2

--- Seccion 3/js-repaso/fatfn.js
+++ Seccion 3/js-repaso/fatfn.js
@@ -13,5 +13,18 @@ function Fn () {
     this.prop = 'propiedad'
     return 'chanchito feliz'
 }
+Fn.prototype.lala = function FuncionDePrototipo () {}
 
 const r1 = new Fn()
+console.log(r1.__proto__)
+
+// la palabra reservada de class es azucar sintactica, lo que hace por detras es crearnos una funcion con esas caracteristicas, cuando nosotros creamos los metodos y propiedades las hace de esa manera y en el caso de las funciones o metedotos los hace de la manera de prototipos, esta es la principal funcionalidad.
+// las funciones creadas con las palabras reservada 'function', pueden ser funciones normales, o clases y tienen contexto de this.
+
+const fatFn = () => {
+    return 'chanchito feliz'
+}
+
+const r2 = fatFn()
+
+// Si uso la palabra new en fatFn apareceria un error. Y no tienen contxto de this. 
