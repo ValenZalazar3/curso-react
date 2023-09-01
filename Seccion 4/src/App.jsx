11499b1e270a536b9400ad33e440f114b408b965
Valen <valenzalazar3@gmail.com>
2023-09-01 00:41:00 -0300

    componentes

--- Seccion 4/src/App.jsx
+++ Seccion 4/src/App.jsx
@@ -1,30 +1,57 @@
-import Button from "./Button.jsx";
+import { Component } from 'react'
 
 
-const arr = [
-  'chanchito feliz',
-  'chanchito triste',
-  'chanchito emocionado',
-]
-
-
-const App = () => {
-
-  const miVariable = false
-  if(miVariable){
-    return <p>Mi variable dio true</p>
+class Button extends Component {
+  state = {}
+  constructor(props){
+    super(props)
+    console.log('constructor', props)
   }
-  return(
-    <div>
-      <h1 onClick={(e) => console.log('click', e)}>Hola mundo</h1>
-      {arr.map((el)=> <p key={el}>{el}</p>)}
-     <Button onClick={() => console.log('Clickeado NASH')} > 
-     Enviar
-     </Button>
-    </div>
-  )
+
+  componentDidMount(){
+    console.log('componentDidMount')
+  }
+
+  componentDidUpdate(prevProp, prevState){
+    console.log('componenDidUpdate', prevProp, prevState)
+
+  }
+
+  componentWillUnmount(){
+    console.log('Desmontando el compoenente', this.props, this.state)
+  }
+  
+  render(){
+    console.log('ejecutando metodo de render de button')
+    return(
+      <button onClick={() => this.setState({ prop: 1})}>
+        Enviar
+      </button>
+    )
+  }
 }
 
 
+class App extends Component {
+
+  state = {
+    valor: 3
+  }
+
+  render(){
+    console.log(this.state)
+    return(
+      <div>
+        <p> Hola Mundo</p>
+        {this.state.valor === 3 ?
+        <Button 
+        chanchito= 'Feliz'/> : null}
+        <button onClick={() => this.setState ({valor: 2}) }>
+        Enviar en App
+        </button>
+      </div>
+    )
+  }
+}
 
 export default App
