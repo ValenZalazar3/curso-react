5cd755271203ed65b8a613cda01072c9a1fe702e
Valentin zalazar <valenzalazar3@gmail.com>
2023-09-14 18:57:59 -0300

    form

---
+++ Seccion-8/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+
+function App() {
+ 
+  const submit = (e) => {
+    e.preventDefault()
+   const data = Array.from(new FormData(e.target))
+   console.log(Object.fromEntries(data))
+  }
+
+  return (
+    <form onSubmit={submit}>
+
+  <div>
+    <span>
+      lala
+    </span>
+    <input name='campo' />
+  </div>
+    <input name='campo-2' />
+    <input type='submit'value='Enviar' />
+
+    </form>
+  )
+}
+
+export default App
